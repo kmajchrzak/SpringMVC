9233e6a587399a908453b5f1be65653d1cf9c175
Karolina Majchrzak <karolina.majchrzak966@gmail.com>
2023-03-18 11:37:27 +0100

    initial commit

---
+++ src/test/java/com/example/Spring/MVC/project/demo/SpringMvcProjectDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring.MVC.project.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcProjectDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
